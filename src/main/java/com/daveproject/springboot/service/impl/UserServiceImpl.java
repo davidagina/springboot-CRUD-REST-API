baddc53d14eacb9cc7e1d95cb25d60d9f3ad6d2e
davidagina <davidagina64@gmail.com>
2023-10-11 22:19:10 +0100

    Added UserServiceImpl class

---
+++ src/main/java/com/daveproject/springboot/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.daveproject.springboot.service.impl;
+
+import com.daveproject.springboot.entity.User;
+import com.daveproject.springboot.repository.UserRepository;
+import com.daveproject.springboot.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+
+    private UserRepository userRepository;
+
+    @Override
+    public User creatUser(User user) {
+
+        return userRepository.save(user);
+    }
+}
