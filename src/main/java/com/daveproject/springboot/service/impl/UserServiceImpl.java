bab8480899bd3b8c3b52a47aa3885c941ecb02d7
davidagina <davidagina64@gmail.com>
2023-10-12 07:10:25 +0100

    Refactored getUserById type to UserDto

--- src/main/java/com/daveproject/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/daveproject/springboot/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.daveproject.springboot.service.impl;
 
 import com.daveproject.springboot.dto.UserDto;
 import com.daveproject.springboot.entity.User;
+import com.daveproject.springboot.mapper.UserMapper;
 import com.daveproject.springboot.repository.UserRepository;
 import com.daveproject.springboot.service.UserService;
 import lombok.AllArgsConstructor;
@@ -20,21 +21,23 @@ public class UserServiceImpl implements UserService {
     public UserDto createUser(UserDto userDto) {
 
         // Convert userDto to user JPA entity object
-        User user = new User(userDto.getId(), userDto.getFirstName(),
-                userDto.getLastName(), userDto.getEmail());
+        User user = UserMapper.mapToUser(userDto);
 
         User savedUser = userRepository.save(user);
 
         // Convert user JPA entity into userDto
-        UserDto savedUserDto = new UserDto(user.getId(), user.getFirstName(),
-                user.getLastName(), user.getEmail());
+        UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+
 
         return savedUserDto;
     }
 
     @Override
-    public User getUserById(Long id) {
-        return userRepository.findById(id).get();
+    public UserDto getUserById(Long id) {
+
+        User user = userRepository.findById(id).get();
+
+       return UserMapper.mapToUserDto(user);
     }
 
     @Override
