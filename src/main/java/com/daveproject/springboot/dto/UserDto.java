2ae4aa06a842e507d37082185d41f23c82f68c8b
davidagina <davidagina64@gmail.com>
2023-10-12 05:31:02 +0100

    Added UserDto class

---
+++ src/main/java/com/daveproject/springboot/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.daveproject.springboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
