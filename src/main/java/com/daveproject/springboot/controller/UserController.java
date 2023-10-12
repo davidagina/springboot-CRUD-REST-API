33b212bfe9790ba0181b33452adc89dea4436f84
davidagina <davidagina64@gmail.com>
2023-10-12 08:19:26 +0100

    Refactored UpdateUser to use Dto

--- src/main/java/com/daveproject/springboot/controller/UserController.java
+++ src/main/java/com/daveproject/springboot/controller/UserController.java
@@ -41,9 +41,9 @@ public class UserController {
 
     // Update user REST API
     @PutMapping("{id}")
-    public ResponseEntity<User> updateUser(@PathVariable("id") Long userId, @RequestBody  User user){
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId, @RequestBody  UserDto user){
         user.setId(userId);
-        User updatedUser = userService.updateUser(user);
+        UserDto updatedUser = userService.updateUser(user);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
