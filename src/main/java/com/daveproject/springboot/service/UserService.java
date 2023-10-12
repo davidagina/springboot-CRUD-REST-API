499dc76dc2583aeeb99bc749a469a141430289a2
davidagina <davidagina64@gmail.com>
2023-10-12 02:43:30 +0100

    Added getAllUsers

--- src/main/java/com/daveproject/springboot/service/UserService.java
+++ src/main/java/com/daveproject/springboot/service/UserService.java
@@ -2,9 +2,13 @@ package com.daveproject.springboot.service;
 
 import com.daveproject.springboot.entity.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User creatUser(User user);
 
     User getUserById(Long id);
+
+    List<User> getAllUsers();
 }
