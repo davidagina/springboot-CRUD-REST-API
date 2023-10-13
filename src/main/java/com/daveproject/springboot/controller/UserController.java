fcb12ec2030771d08fed4a944de71dbd36b4f8b9
davidagina <davidagina64@gmail.com>
2023-10-13 18:13:04 +0100

    Added handleResourceNotFoundException

--- src/main/java/com/daveproject/springboot/controller/UserController.java
+++ src/main/java/com/daveproject/springboot/controller/UserController.java
@@ -2,13 +2,17 @@ package com.daveproject.springboot.controller;
 
 import com.daveproject.springboot.dto.UserDto;
 import com.daveproject.springboot.entity.User;
+import com.daveproject.springboot.exception.ErrorDetails;
+import com.daveproject.springboot.exception.ResourceNotFoundException;
 import com.daveproject.springboot.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -53,5 +57,18 @@ public class UserController {
         userService.deleteUser(id);
         return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
     }
+
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+//                                                                        WebRequest webRequest){
+//        ErrorDetails errorDetails = new ErrorDetails(
+//                LocalDateTime.now(),
+//                exception.getMessage(),
+//                webRequest.getDescription(false),
+//                "USER_NOT_FOUND"
+//        );
+//
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
 
 }
