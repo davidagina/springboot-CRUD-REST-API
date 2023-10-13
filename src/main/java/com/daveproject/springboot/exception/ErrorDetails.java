e4863e1921ed4fdcb1cd2195cf17169893db33d9
davidagina <davidagina64@gmail.com>
2023-10-13 18:13:57 +0100

    Updated

--- src/main/java/com/daveproject/springboot/exception/ErrorDetails.java
+++ src/main/java/com/daveproject/springboot/exception/ErrorDetails.java
@@ -1,4 +1,20 @@
 package com.daveproject.springboot.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorDetails {
+
+    private LocalDateTime timeStamp;
+    private String message;
+    private String path;
+    private String errorCode;
 }
