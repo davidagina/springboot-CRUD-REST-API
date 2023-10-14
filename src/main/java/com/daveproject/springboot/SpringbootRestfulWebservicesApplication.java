ea418e8e559470e9552217b7af539c1e61166fa7
davidagina <davidagina64@gmail.com>
2023-10-14 23:04:16 +0100

    Added general api info using annotation

--- src/main/java/com/daveproject/springboot/SpringbootRestfulWebservicesApplication.java
+++ src/main/java/com/daveproject/springboot/SpringbootRestfulWebservicesApplication.java
@@ -1,11 +1,37 @@
 package com.daveproject.springboot;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation",
+				description = "Spring Boot REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "David C. Agina",
+						email = "david-agina@outlook.com",
+						url = "https://github.com/davidagina"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/davidagina"
+				)
+
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management Documentation",
+				url = "https://github.com/davidagina"
+		)
+)
 public class SpringbootRestfulWebservicesApplication {
 
 	@Bean
